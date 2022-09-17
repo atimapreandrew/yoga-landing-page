c24fc3c8a7a7aca3ef2560585f3764b101f9e883
Andrew Atimapre <atimaxandrew@gmail.com>
2022-09-17 15:36:11 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Axios from "axios";
+import Hero from "./components/Hero";
+import Cards from "./components/Cards";
+import Facts from "./components/Facts";
+import Features from "./components/Features";
+import Courses from "./components/Courses";
+import Pricing from "./components/Pricing";
+import Newsletter from "./components/Newsletter";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return <div className="overflow-hidden"></div>;
+}
+
+export default App;
